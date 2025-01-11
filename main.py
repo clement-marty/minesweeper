b869c28c9954f44b699d7cb34dbea2efc36a76ab
Clément MARTY <clement_marty@outlook.fr>
2025-01-11 19:08:17 +0100

    Add sound effects to the game and a volume setting in config.ini

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def init_config() -> None:
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    global GRID_WIDTH, GRID_HEIGHT, CELL_SIZE, GAME_FPS, MINE_COUNT
+    global GRID_WIDTH, GRID_HEIGHT, MINE_COUNT, CELL_SIZE, GAME_FPS, SFX_VOLUME
     GRID_WIDTH      = config.getint('grid', 'width')
     GRID_HEIGHT     = config.getint('grid', 'height')
+    MINE_COUNT      = config.getint('game', 'mine_count')
     CELL_SIZE       = config.getint('renderer', 'cell_size')
     GAME_FPS        = config.getint('renderer', 'frames_per_second')
-    MINE_COUNT      = config.getint('game', 'mine_count')
+    SFX_VOLUME      = config.getfloat('audio', 'sound_effects_volume')
 
 
 
@@ -28,6 +29,7 @@ def game_loop() -> None:
     '''Main game loop'''
     init_config()
     pygame.init()
+    pygame.mixer.init()
     pygame.font.init()
 
     screen = pygame.display.set_mode((
@@ -42,6 +44,7 @@ def game_loop() -> None:
     number_font = pygame.font.SysFont('Arial', CELL_SIZE, bold=True)
     text_font = pygame.font.SysFont('Arial', CELL_SIZE*GRID_WIDTH//10, bold=True)
     textures = Assets.Textures(CELL_SIZE, number_font)
+    sound_effects = Assets.SoundEffects(SFX_VOLUME)
 
 
     clock = pygame.time.Clock()
@@ -69,12 +72,18 @@ def game_loop() -> None:
                         if first_click: # If it is the first clock, generate the grid
                             grid, neighbours_grid, discovered_grid = GameLogic.create_from_coords(x, y, (GRID_WIDTH, GRID_HEIGHT), MINE_COUNT)
                             first_click = False
+                            sound_effects.cell_discovered.play()
 
                         elif GameLogic.discover_cell(grid, neighbours_grid, discovered_grid, x, y)[0]:
                             has_lost = True
+                            sound_effects.bomb_explodes.play()
+                        
+                        else:
+                            sound_effects.cell_discovered.play()
 
                     elif event.button == 3: # The right mouse button
                         GameLogic.flag_cell(discovered_grid, x, y)
+                        sound_effects.flag_placed.play()
 
         # Check for a win
         if GameLogic.check_win(grid, discovered_grid, MINE_COUNT):
